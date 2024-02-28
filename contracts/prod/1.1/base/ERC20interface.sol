ef17954ac2a7501c10e829599ebb4396cb940e0e
GitSwarm <115565391+GitSwarmOrg@users.noreply.github.com>
2024-02-28 16:55:35 +0200

    update contracts pragmas to non-floating

--- contracts/prod/1.1/base/ERC20interface.sol
+++ contracts/prod/1.1/base/ERC20interface.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
 interface ERC20interface {
     function decimals() external view returns (uint dec);
--- contracts/test/Proposal2.sol
+++ contracts/test/Proposal2.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
 import "../prod/1.1/Proposal.sol";
 
